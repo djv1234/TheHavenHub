76aa4e2b17b26132624afa02958a22bf1dce64f8
DmitryVolf <volf.dmitry@gmail.com>
2025-01-25 00:48:25 -0500

    Started implementation of API for healthView, made medicalservices and healthviewmodel folders to hold information for HealthView

---
+++ HavenHub/Views/HealthView.swift
@@ -0,0 +1,18 @@
+//
+//  HealthView.swift
+//  HavenHub
+//
+//  Created by Dmitry Volf on 1/24/25.
+//
+
+import SwiftUI
+
+struct HealthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthView()
+}
