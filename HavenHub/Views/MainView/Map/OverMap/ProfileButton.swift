474efb485c857864372c9a687af2b4db67d9f42d
DmitryVolf <volf.dmitry@gmail.com>
2025-04-30 13:33:46 -0400

    made "Coming soon!" for profile allowing us to include the view

--- HavenHub/Views/MainView/Map/OverMap/ProfileButton.swift
+++ HavenHub/Views/MainView/Map/OverMap/ProfileButton.swift
@@ -5,26 +5,28 @@
 //  Created by Garrett Butchko on 1/23/25.
 //
 
-//import SwiftUI
-//
-//struct ProfileButton: View {
-//    @StateObject var viewManager: ViewManager
-//    @Binding var showSheet: Bool
-//    
-//    var body: some View {
-//        Button(action: {
-//            viewManager.navigateToProfile()
-//            showSheet = true
-//        }) {
-//            ZStack {
-//                Circle()
-//                 .fill(.ultraThinMaterial)
-//                   .frame(width: 40, height: 40)
-//                Image(systemName: "person.fill")
-//                    .resizable()
-//                    .foregroundColor(.primary)
-//                    .frame(width: 20, height: 20)
-//            }
-//        }
-//    }
-//}
+import SwiftUI
+
+struct ProfileButton: View {
+    @StateObject var viewManager: ViewManager
+    @Binding var showSheet: Bool
+    
+    var body: some View {
+        Button(action: {
+            withAnimation{
+                viewManager.navigateToProfile()
+                showSheet = true
+            }
+        }) {
+            ZStack {
+                Circle()
+                 .fill(.ultraThinMaterial)
+                   .frame(width: 40, height: 40)
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .foregroundColor(.primary)
+                    .frame(width: 20, height: 20)
+            }
+        }
+    }
+}
--- HavenHub/Views/ProfileView/ProfileView.swift
+++ HavenHub/Views/ProfileView/ProfileView.swift
@@ -9,84 +9,147 @@ struct ProfileView: View {
     @State private var user : UserModel = UserModel(name: "", email: "", password: "")
     
     var body: some View {
-        HStack {
+//        HStack {
 //            Capsule()
 //                .frame(width: 38, height: 6)
 //                .foregroundColor(.gray)
-//                .padding(10)
-            Button(
-                action: {
-                withAnimation {
-                    viewManager.navigateToMain()
-                }
-            }) {
-                ZStack {
-                    Circle()
-                        .fill(Color.red)
-                        .frame(width: 36, height: 36)
-                    Image(systemName: "arrow.left")
-                        .foregroundColor(.white)
-                        .font(.system(size: 16, weight: .bold))
-                }
-            }
+//               .padding(10)
+//            Button(
+//                action: {
+//                withAnimation {
+//                    viewManager.navigateToMain()
+//                }
+//            }) {
+//                ZStack {
+//                    Circle()
+//                        .fill(Color.red)
+//                        .frame(width: 36, height: 36)
+//                    Image(systemName: "arrow.left")
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 16, weight: .bold))
+//                }
+//            }
+//            
+//            Text("Profile")
+//                .frame(width: 250, height: 40)
+//                .font(.title)
+//                .foregroundColor(.primary)
+//                .fontWeight(.bold)
+//        }
+        
+        ZStack{
+            // Background overlay
+            Rectangle()
+                .fill(.ultraThinMaterial) // Semi-transparent background for focus
+                .ignoresSafeArea() // Extend the background to the edges of the screen
+                .frame(maxWidth: .infinity, maxHeight: .infinity) // Full screen coverage
             
-            Text("Profile")
-                .frame(width: 250, height: 40)
-                .font(.title)
-                .foregroundColor(.primary)
-                .fontWeight(.bold)
+            VStack {
+                ZStack{
+                    HStack{
+                        Button(
+                            action: {
+                                withAnimation {
+                                    viewManager.navigateToMain()
+                                }
+                            }) {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color.red)
+                                    // .frame(width: 36, height: 36)
+                                    Image(systemName: "arrow.left")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 16, weight: .bold))
+                                }
+                            }
+                            .frame(width: 40, height: 40)
+                            .padding(.leading)
+                            .padding(.top)
+                        Spacer()
+                    }
+                    
+                    Text("Profile")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.primary)
+                                            .frame(maxWidth: .infinity)
+                                            .multilineTextAlignment(.center)
+                                            .padding(.top)
+                    Spacer()
+
+                                        // Invisible view to balance the back button
+                                        // Keeps "Profile" centered
+                                        Color.clear
+                                            .frame(width: 40, height: 40)
+                                            .padding(.trailing)
+                                            .padding(.top)
+                }
+              //  Spacer()
+                Spacer()
+                
+                            Text("Coming soon!")
+                                .fontWeight(.bold)
+                                .font(.largeTitle)
+                                .padding()
+                                .foregroundColor(.primary)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                Spacer()
+
+                        }
+
         }
         
-        List {
-            HStack {
-                Text("Name:")
-                if !editProfile {
-                    Text(user.name) // Use @State variable
-                } else {
-                    TextField("Name", text: $user.name) // Bind to @State variable
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .background(.ultraThinMaterial)
-                }
-            }
-            
-            Text("Email: \(user.email)") // Show email from @State
-            
-            if authViewModel.user != nil {
-                if !editProfile {
-                    Button("Edit Profile") {
-                        editProfile = true
-                    }
-                } else {
-                    Button("Save") {
-                        editProfile = false
-                        authViewModel.saveUserData(user: user) { _ in }
-                    }
-                }
-                
-                Button("Logout") {
-                    withAnimation {
-                        viewManager.navigateToLogin()
-                    }
-                    authViewModel.logout()
-                }
-            } else {
-                Button(action: {
-                    withAnimation {
-                        viewManager.navigateToLogin()
-                    }
-                }) {
-                    Text("Login")
-                }
-            }
-        }
-        .onAppear {
-            authViewModel.fetchUserData() { user in
-                if let newUser = user {
-                    self.user = newUser
-                }
-            }
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.leading)
+//        List {
+//            HStack {
+//                Text("Name:")
+//                if !editProfile {
+//                    Text(user.name) // Use @State variable
+//                } else {
+//                    TextField("Name", text: $user.name) // Bind to @State variable
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .background(.ultraThinMaterial)
+//                }
+//            }
+//            
+//            Text("Email: \(user.email)") // Show email from @State
+//            
+//            if authViewModel.user != nil {
+//                if !editProfile {
+//                    Button("Edit Profile") {
+//                        editProfile = true
+//                    }
+//                } else {
+//                    Button("Save") {
+//                        editProfile = false
+//                        authViewModel.saveUserData(user: user) { _ in }
+//                    }
+//                }
+//                
+//                Button("Logout") {
+//                    withAnimation {
+//                        viewManager.navigateToLogin()
+//                    }
+//                    authViewModel.logout()
+//                }
+//            } else {
+//                Button(action: {
+//                    withAnimation {
+//                        viewManager.navigateToLogin()
+//                    }
+//                }) {
+//                    Text("Login")
+//                }
+//            }
+//        }
+//        .onAppear {
+//            authViewModel.fetchUserData() { user in
+//                if let newUser = user {
+//                    self.user = newUser
+//                }
+//            }
+//        }
+//        .frame(maxWidth: .infinity, alignment: .leading)
+//        .padding(.leading)
     }
 }
